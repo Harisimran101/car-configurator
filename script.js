07e6d968aa0c2d63f994b56897147844551bd2aa
Harisimran101 <harisimran1012@gmail.com>
2022-03-22 11:50:37 +0500

    update-3

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.136';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/loaders/DRACOLoader.js';
+
 import { RGBELoader } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/loaders/RGBELoader.js';
 // import { PMREMGenerator } from 'https://cdn.skypack.dev/three@0.136/examples/src/extras/PMREMGenerator.js'
 
 //scene
+function init(){
 
 const scene = new THREE.Scene()
 
@@ -24,7 +27,22 @@ scene.add(camera)
 
 const canvas = document.querySelector('.webgl')
 
+//renderer
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+
+})
+renderer.setSize(size.with, size.height)
+renderer.setPixelRatio(window.devicePixelRatio * 2);
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 0.85;
+
 // Texture Loader 
+
+
 
 const textureloader = new THREE.TextureLoader();
 let groundshadow = textureloader.load('images/ground-shadow.png');
@@ -40,36 +58,65 @@ const carmaterial = new THREE.MeshPhysicalMaterial({
 
 //hdr texture
 
+let envtexture;
+
 const rgb = new RGBELoader()
 rgb.load(
-  'hdr2.hdr',
+  'environment3.hdr',
   (texture) => {
       texture.mapping = THREE.EquirectangularReflectionMapping,
-      scene.environment = texture;
       scene.background = texture;
+      envtexture = texture;
+    
+     
   }
 )
 
+function updatematerials() {
+  scene.traverse((child) =>{
+     if(child instanceof THREE.Mesh){
+        child.material.envMap = envtexture;
+        child.material.envMapIntensity = 1.7;
+
+     };
+  })
+}
+
+
+const dracoloader = new DRACOLoader();
+dracoloader.setDecoderPath( '/draco/' );
 
 let body;
 let model;
-const loader = new GLTFLoader()
-loader.load(
-  'car-1.glb',
+
+const gltfloader = new GLTFLoader()
+gltfloader.setDRACOLoader(dracoloader);
+
+gltfloader.load(
+  'car-file.gltf',
   (gltf) => {
     model = gltf.scene;
     let ground = model.getObjectByName('Plane');
     let carbody = model.getObjectByName('Car-body');
     carbody.material = carmaterial;
+
     ground.material.map = groundshadow;
+    ground.scale.set(7.3,7.3,7.3);
     model.scale.set(3, 3, 3);
     scene.add(model);
 
     model.traverse(function (child) {
+    
+      child.castShadow = true;
+
       if (child.material && child.material.name === 'carpaint') {
         child.material = carmaterial
       }
     })
+
+    updatematerials();
+
+  
   }
 )
 
@@ -79,15 +126,8 @@ const controls = new OrbitControls(camera, canvas)
 controls.maxPolarAngle = Math.PI / 2;
 controls.enableDamping = true
 
-//renderer
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  antialias: true,
 
-})
-renderer.setSize(size.with, size.height)
-renderer.setPixelRatio(window.devicePixelRatio);
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -114,3 +154,7 @@ const tick = () => {
 
 }
 tick()
+
+};
+
+init();
